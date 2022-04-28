a945c81d86d35670bf6ceb55f3e5d00a77da2e83
Camila Bernales <camila.bernales@digiventures.la>
2022-04-28 20:03:16 -0300

    first commit

---
+++ components/PlayerLoyout.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/layout'
+import React from 'react'
+
+const PlayerLoyout = ({children}) => {
+  return (
+    <Box>
+        Layout
+      {children}
+    </Box>
+  )
+}
+
+export default PlayerLoyout
---
+++ pages/_app.tsx
@@ -0,0 +1,43 @@
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import PlayerLoyout from "../components/PlayerLoyout";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <PlayerLoyout>
+        <Component {...pageProps} />
+      </PlayerLoyout>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
