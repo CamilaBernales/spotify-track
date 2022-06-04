7b10d50a65162e44f55bd4f25dab559d2836d0f2
camila bernales <camilabernalespersonal@gmail.com>
2022-06-03 22:38:32 -0300

    feat: add schema

--- components/PlayerLoyout.tsx
+++ components/PlayerLoyout.tsx
@@ -1,11 +1,28 @@
-import { Box } from '@chakra-ui/layout'
 import React from 'react'
+import {
+  Box,
+  List,
+  ListIcon,
+  ListItem,
+  Divider,
+  Center,
+  Link,
+  LinkBox,
+  LinkOverlay
+} from "@chakra-ui/layout"
+
+import Sidebar from './Sidebar'
 
 const PlayerLoyout = ({children}) => {
   return (
-    <Box>
-        Layout
-      {children}
+    <Box width="100vw" height="100vh">
+       <Box position="absolute" top="0" width="250px" left="0">
+         <Sidebar />
+       </Box>
+       <Box width="250px" marginBottom="100px">
+        {children}
+        <Box position="absolute" left="0" bottom="0"></Box>
+       </Box>
     </Box>
   )
 }
