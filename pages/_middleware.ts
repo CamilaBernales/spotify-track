5e82ecd72de9ed2e65c9b24768d36bf3e33636b0
CamilaBernales <camilabernales09@gmail.com>
2022-09-01 18:34:06 -0300

    feat: add edge function

---
+++ pages/_middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server"
+
+const signedinPages = ['/', '/playlist', '/library']
+
+export default function middleware(req, res, next) { 
+    if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+        const token = req.cookies.TRAX_ACCESS_TOKEN
+        if(!token){
+            return NextResponse.redirect('/signin')
+        }
+    }
+}
